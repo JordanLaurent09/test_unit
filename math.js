73902a423430371ff6cf87b7b275841089b8d10d
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2025-04-08 09:50:23 +0300

    created funcs multi, substract

--- math.js
+++ math.js
@@ -4,8 +4,23 @@ function returnAddComponents (a, b) {
 }
 
 function returnDivideComponents (a, b) {
-
+    if ( b === 0 ) throw Error('Division by zero');
     return a / b;
 }
 
-module.exports = { returnAddComponents, returnDivideComponents };
+function returnMultiplyComponents (a, b) {
+
+    return a * b;
+}
+
+function returnSubstractComponents (a, b) {
+
+    return a - b;
+}
+
+module.exports = { 
+    returnAddComponents, 
+    returnDivideComponents,
+    returnMultiplyComponents,
+    returnSubstractComponents
+};
